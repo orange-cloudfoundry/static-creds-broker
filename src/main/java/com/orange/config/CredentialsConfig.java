7c18db8dbaa9b749b1acf5e6675dbd97822c62d2
tao-xinxiu <isabelle.tao@orange.com>
2016-02-12 11:09:43 +0100

    add ability to parse user self-defined credential key_value in SERVICES_ID_CREDENTIALS_MYOWNKEY, example in manifest.yml

--- src/main/java/com/orange/config/CredentialsConfig.java
+++ src/main/java/com/orange/config/CredentialsConfig.java
@@ -1,5 +1,6 @@
 package com.orange.config;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -7,6 +8,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.orange.model.Credentials;
 
 @Configuration
@@ -16,7 +19,7 @@ public class CredentialsConfig {
 	@Value("#{ systemEnvironment['SERVICES_ID_CREDENTIALS_HOSTNAME'] }")
 	private String credentials_hostname;
 	@Value("#{ systemEnvironment['SERVICES_ID_CREDENTIALS_MYOWNKEY'] }")
-	private String credentials_myownkey;
+	private String credentials_myownkey_str;
 	
 	@Bean
 	public Credentials credentials(){
@@ -27,8 +30,20 @@ public class CredentialsConfig {
 		if (credentials_hostname != null) {
 			credentials.put("hostname", credentials_hostname);
 		}
-		if (credentials_myownkey != null) {
-			credentials.put("myownkey", credentials_myownkey);
+		if (credentials_myownkey_str != null) {
+			if (!credentials_myownkey_str.startsWith("{")) { // my_own_key is no a json hash
+				credentials.put("myownkey", credentials_myownkey_str);
+			}
+			else {
+				Map<String, Object> myownkey_map = new HashMap<>();
+				ObjectMapper mapper = new ObjectMapper();
+				try {
+					myownkey_map = mapper.readValue(credentials_myownkey_str, new TypeReference<Map<String, String>>(){});
+					credentials.putAll(myownkey_map);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
 		}
 		return new Credentials(credentials);
 	}
