e24e90d09d833d42e8dbfab4be4c130a01a6b030
tao-xinxiu <isabelle.tao@orange.com>
2016-02-19 11:28:19 +0100

    change the obtention of test credential info from cfg file to system environment variables for integration with concourse

--- acceptance/Configuration.py
+++ acceptance/Configuration.py
@@ -1,26 +1,14 @@
 import os
-from ConfigParser import ConfigParser
 
-path = os.path.join(os.path.dirname(__file__), 'acceptance.cfg')
-if not (os.path.exists(path) and os.path.isfile(path) and os.access(path, os.R_OK)):
-    raise IOError('property must be a valid path: %s' % path)
-cfg = ConfigParser()
-cfg.read(path)
+ORGANIZATION_NAME = os.environ["organization_name"].replace("\r","")
+SPACE_NAME = os.environ["space_name"].replace("\r","")
+SERVICE_NAME = os.environ["service_name"].replace("\r","")
+PLAN_NAME = os.environ["plan_name"].replace("\r","")
+TESTED_APP_NAME = os.environ["application_name"].replace("\r","")
+SERVICE_INSTANCE_NAME = os.environ["service_instance_name"].replace("\r","")
 
-ORGANIZATION_NAME = cfg.get("information", "organization_name")
-SPACE_NAME = cfg.get("information", "space_name")
-SERVICE_NAME = cfg.get("information", "service_name")
-PLAN_NAME = cfg.get("information", "plan_name")
-TESTED_APP_NAME = cfg.get("information", "application_name")
-SERVICE_INSTANCE_NAME = cfg.get("information", "service_instance_name")
+CLIENT_ENDPOINT = os.environ["cf_target_endpoint"].replace("\r","")
+CLIENT_SKIP_SSL = os.environ["skip_ssl_verification"].replace("\r","").lower() == "true"
+CLIENT_USER = os.environ["cf_username"].replace("\r","")
+CLIENT_PASSWORD = os.environ["cf_password"].replace("\r","")
 
-
-CLIENT_ENDPOINT = cfg.get("client", "target_endpoint")
-CLIENT_SKIP_SSL = cfg.get("client", "skip_ssl_verification").lower() == "true"
-CLIENT_USER = cfg.get("client", "username")
-CLIENT_PASSWORD = cfg.get("client", "password")
-
-
-# AUTOSLEEP_ENDPOINT = cfg.get("broker", "target_endpoint")
-# USER_NAME = cfg.get("broker", "user")
-# USER_PASSWORD = cfg.get("broker", "password")
