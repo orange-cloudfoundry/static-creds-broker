1622c35724e4dd7422f5ef9bc93acef997e304b5
tao-xinxiu <isabelle.tao@orange.com>
2016-02-22 13:40:02 +0100

    Revert to use spring-cloud-cloudfoundry-service-broker release version(1.0.0.RC1) instead of latest version "remove support broker API version(ANY) as it's set default by spring-cloud-cloudfoundry-service-broker in commit 09886498fd872fd7c97dca83916d773ce8ead68b"
    This reverts commit 7fe73208a99bd88a1cfd8d023a042b047a171461.

--- src/main/java/com/orange/Application.java
+++ src/main/java/com/orange/Application.java
@@ -5,7 +5,10 @@ import java.util.Map;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.servicebroker.model.BrokerApiVersion;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import static org.springframework.cloud.servicebroker.model.BrokerApiVersion.API_VERSION_ANY;
 
 @EnableGlobalMethodSecurity
 @SpringBootApplication(scanBasePackages = { "com.orange" })
@@ -22,5 +25,10 @@ public class Application {
 		app.setDefaultProperties(properties);
 		app.run(args);
 //		SpringApplication.run(Application.class, args);
+	}
+	
+	@Bean
+	public BrokerApiVersion brokerApiVersion() {
+		return new BrokerApiVersion(API_VERSION_ANY);
 	}
 }
