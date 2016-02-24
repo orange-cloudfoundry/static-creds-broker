8f9d4fa35e998b3dc85e9d75d2a882cdd0d82598
tao-xinxiu <isabelle.tao@orange.com>
2016-02-24 14:56:20 +0100

    add func to check whether mandatory properties defined, add check for SECURITY_PASSWORD

--- src/main/java/com/orange/Application.java
+++ src/main/java/com/orange/Application.java
@@ -20,19 +20,43 @@ public class Application {
 		app.run(args);
 //		SpringApplication.run(Application.class, args);
 	}
-	
+
 	@Bean
 	public BrokerApiVersion brokerApiVersion() {
 		return new BrokerApiVersion(API_VERSION_ANY);
 	}
-	
-	public static Map<String, Object> getSecurityProperties(){
+
+	/**
+	 * get SecurityProperties for system environment variables
+	 * 
+	 * @return SecurityProperties which contains security.user.name and
+	 *         security.user.password
+	 */
+	public static Map<String, Object> getSecurityProperties() {
 		Map<String, Object> properties = new HashMap<String, Object>();
 		Map<String, String> env = System.getenv();
 		String username = env.get("SECURITY_USER");
 		String password = env.get("SECURITY_PASSWORD");
+		Map<String, Object> mandatoryProperties = new HashMap<String, Object>();
+		mandatoryProperties.put("SECURITY_PASSWORD", password);
+		checkMandatoryPropertiesDefined(mandatoryProperties);
 		properties.put("security.user.name", username);
 		properties.put("security.user.password", password);
 		return properties;
+	}
+
+	/**
+	 * throw IllegalArgumentException when the mandatory property
+	 * (SECURITY_PASSWORD) is not set
+	 * 
+	 * @throws IllegalArgumentException
+	 */
+	public static void checkMandatoryPropertiesDefined(Map<String, Object> mandatoryProperties)
+			throws IllegalArgumentException {
+		for (Map.Entry<String, Object> mandatoryProperty : mandatoryProperties.entrySet()) {
+			if (mandatoryProperty.getValue() == null) {
+				throw new IllegalArgumentException("Mandatory property: " + mandatoryProperty.getKey() + " missing");
+			}
+		}
 	}
 }
