01fb7c87b635822e73d2c66c5d070e9325390107
tao-xinxiu <isabelle.tao@orange.com>
2016-03-15 15:39:11 +0100

    implement parsePlansProperties and parseCredentialsProperties(json str credentials not yet supported) with its unit tests; implement other func in ParserProperties interface

--- src/main/java/com/orange/util/ParserApplicationProperties.java
+++ src/main/java/com/orange/util/ParserApplicationProperties.java
@@ -3,6 +3,7 @@ package com.orange.util;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,6 +13,8 @@ import com.orange.model.*;
 @ConfigurationProperties
 public class ParserApplicationProperties implements ParserProperties{
 	private Map<String, Object> services = new HashMap<>();
+	@Value("${security.user.password}")
+	private String password;
 
 	public Map<String, Object> getServices() {
 		return services;
@@ -55,10 +58,29 @@ public class ParserApplicationProperties implements ParserProperties{
 	@Override
 	public PlansMap parsePlansProperties(String serviceID) {
 		PlansMap plansMap = new PlansMap();
-//		Object serviceProperties = services.get(serviceID);
-//		if (serviceProperties instanceof Map<?, ?>) {
-////			Map<?, ?> servicePropertiesMap = 
-//		}
+		Object serviceProperties = services.get(serviceID);
+		if (serviceProperties instanceof Map<?, ?>) {
+			Map<?, ?> servicePropertiesMap = (Map<?, ?>)serviceProperties;
+			Object plansProperties = servicePropertiesMap.get("PLAN");
+			if (plansProperties instanceof Map<?, ?>) {
+				Map<?, ?> plansPropertiesMap = (Map<?, ?>)plansProperties;
+				for(Map.Entry<?, ?> entry: plansPropertiesMap.entrySet()){
+					if (entry.getKey() instanceof String) {
+						String planID = (String)entry.getKey();
+						Object planProperties = entry.getValue();
+						if (planProperties instanceof Map<?, ?>) {
+							Map<?, ?> planPropertiesMap = (Map<?, ?>)planProperties;
+							for (PlanPropertyName planPropertyName : PlanPropertyName.values()) {
+								Object planPropertyValue = planPropertiesMap.get(planPropertyName.toString());
+								if (planPropertyValue != null) {
+									plansMap.addPlanProperty(planID, planPropertyName, planPropertyValue.toString());
+								}
+							}
+						}
+					}
+				}
+			}
+		}
 		plansMap.checkPlansNameNotDuplicated();
 		plansMap.setPlansPropertiesDefaults();
 		return plansMap;
@@ -66,29 +88,84 @@ public class ParserApplicationProperties implements ParserProperties{
 
 	@Override
 	public CredentialsMap parseCredentialsProperties() {
-		// TODO Auto-generated method stub
-		return new CredentialsMap();
+		CredentialsMap credentialsMap = new CredentialsMap();
+		for (Map.Entry<String, Object> entry : services.entrySet()) {
+			if (entry.getValue() instanceof Map<?, ?>) {
+				Map<?, ?> servicesProperties = (Map<?, ?>) entry.getValue();
+				for (Map.Entry<?, ?> serviceProperties : servicesProperties.entrySet()) {
+					if ("CREDENTIALS".equals(serviceProperties.getKey())) {
+						//TODO yaml could add both value and map ?
+						if (serviceProperties.getValue() instanceof Map<?, ?>) {
+							for (Map.Entry<?, ?> credentialProperty : ((Map<?, ?>)serviceProperties.getValue()).entrySet() ) {
+								credentialsMap.addCredential(entry.getKey(), null, credentialProperty.getKey().toString(), credentialProperty.getValue().toString());
+							}
+						}
+					}
+					if ("PLAN".equals(serviceProperties.getKey())) {
+						if (serviceProperties.getValue() instanceof Map<?, ?>) {
+							for (Map.Entry<?, ?> planProperties : ((Map<?, ?>)serviceProperties.getValue()).entrySet()) {
+								if (planProperties.getValue() instanceof Map<?, ?>) {
+									for (Map.Entry<?, ?> planProperty : ((Map<?, ?>)planProperties.getValue()).entrySet()) {
+										if ("CREDENTIALS".equals(planProperty.getKey())) {
+											if (planProperty.getValue() instanceof Map<?, ?>) {
+												for (Map.Entry<?, ?> credentialProperty : ((Map<?, ?>)planProperty.getValue()).entrySet() ) {
+													credentialsMap.addCredential(entry.getKey(), planProperties.getKey().toString(), credentialProperty.getKey().toString(), credentialProperty.getValue().toString());
+												}
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+		return credentialsMap;
 	}
 
 	@Override
 	public String getServiceName(String serviceID) {
-		// TODO Auto-generated method stub
-		return "";
+		Object serviceProperties = this.services.get(serviceID);
+		if (serviceProperties instanceof Map<?, ?>) {
+			return ((Map<?, ?>)serviceProperties).get("NAME").toString();
+		}
+		return null;
 	}
 
 	@Override
 	public String getPlanName(String serviceID, String planID) {
-		// TODO Auto-generated method stub
-		return "";
+		Object serviceProperties = this.services.get(serviceID);
+		if (serviceProperties instanceof Map<?, ?>) {
+			Object plansProperties = ((Map<?, ?>)serviceProperties).get("PLAN");
+			if (plansProperties instanceof Map<?, ?>) {
+				Object planProperties = ((Map<?, ?>)plansProperties).get(planID);
+				if (planProperties instanceof Map<?, ?>) {
+					return ((Map<?, ?>)planProperties).get("NAME").toString();
+				}
+			}
+		}
+		return null;
 	}
 
 	@Override
 	public void checkPasswordDefined() throws IllegalArgumentException {
-		// TODO Auto-generated method stub
+		if(this.password == null){
+			throw new IllegalArgumentException("Mandatory property: security.user.password missing");
+		}
 	}
 
 	@Override
 	public void checkServiceMandatoryPropertiesDefined(String serviceID) throws IllegalArgumentException {
-		// TODO Auto-generated method stub
+		Object serviceProperties = this.services.get(serviceID);
+		if (serviceProperties instanceof Map<?, ?>) {
+			Map<?, ?> servicePropertiesMap = (Map<?, ?>)serviceProperties;
+			if (servicePropertiesMap.get("NAME") == null) {
+				throw new IllegalArgumentException("Mandatory property: service." + serviceID + ".NAME missing");
+			}
+			if (servicePropertiesMap.get("DESCRIPTION") == null) {
+				throw new IllegalArgumentException("Mandatory property: service." + serviceID + ".DESCRIPTION missing");
+			}
+		}
 	}
 }
--- src/test/java/com/orange/util/ParserApplicationPropertiesTest.java
+++ src/test/java/com/orange/util/ParserApplicationPropertiesTest.java
@@ -6,6 +6,9 @@ import java.util.Map;
 import org.junit.Assert;
 import org.junit.Test;
 
+import com.orange.model.CredentialsMap;
+import com.orange.model.PlanPropertyName;
+import com.orange.model.PlansMap;
 import com.orange.model.ServicePropertyName;
 import com.orange.model.ServicesMap;
 
@@ -23,6 +26,23 @@ public class ParserApplicationPropertiesTest {
 	private static final String TRIPADVISOR_METADATA_DOCUMENTATIONURL = "https://github.com/tao-xinxiu/static-creds-broker";
 	private static final String TRIPADVISOR_METADATA_PROVIDERDISPLAYNAME = "provider display name";
 	private static final String TRIPADVISOR_METADATA_LONGDESCRIPTION = "A long description for my services";
+	private static final String API_DIRECTORY_PLAN_PLAN1_ID = "PLAN1";
+	private static final String API_DIRECTORY_PLAN_PLAN1_NAME = "dev";
+	private static final String API_DIRECTORY_PLAN_PLAN1_DESCRIPTION = "The description of the plan dev";
+	private static final String API_DIRECTORY_PLAN_PLAN2_ID = "PLAN2";
+	private static final String API_DIRECTORY_PLAN_PLAN2_NAME = "preprod";
+	private static final String API_DIRECTORY_PLAN_PLAN2_FREE = "false";
+	private static final String API_DIRECTORY_PLAN_PLAN2_METADATA = ""; //'{"bullets":["20 GB of messages","20 connections"],"costs":[{"amount":{"usd":99.0,"eur":49.0},"unit":"MONTHLY"},{"amount":{"usd":0.99,"eur":0.49},"unit":"1GB of messages over 20GB"}],"displayName":"Big Bunny"}'
+	private static final String API_DIRECTORY_PLAN_PLAN3_ID = "PLAN3";
+	private static final String API_DIRECTORY_PLAN_PLAN3_NAME = "prod";
+	private static final String API_DIRECTORY_SERVICE_CREDENTIALS_HOSTNAME = "http://company.com";
+	private static final String API_DIRECTORY_PLAN_PLAN1_CREDENTIALS_URI = "http://mydev-api.org";
+	private static final String API_DIRECTORY_PLAN_PLAN1_CREDENTIALS_ACCESS_KEY = "devAZERT23456664DFDSFSDFDSF";
+	private static final String API_DIRECTORY_PLAN_PLAN2_CREDENTIALS_URI = "http://mypreprod-api.org";
+	private static final String API_DIRECTORY_PLAN_PLAN2_CREDENTIALS_ACCESS_KEY = "preprodAZERT23456664DFDSFSDFDSF";
+	private static final String API_DIRECTORY_PLAN_PLAN3_CREDENTIALS_URI = "http://myprod-api.org";
+	private static final String API_DIRECTORY_PLAN_PLAN3_CREDENTIALS_ACCESS_KEY = "prodAZERT23456664DFDSFSDFDSF";
+	private static final String TRIPADVISOR_SERVICE_CREDENTIALS_ACCESS_KEY = "AZERT23456664DFDSFSDFDSF";
 			
 	@Test
 	public void should_get_services_map_with_info_have_been_set_in_property() {
@@ -33,6 +53,7 @@ public class ParserApplicationPropertiesTest {
 		Assert.assertEquals(2, servicesMap.geEntrySet().size());
 		Assert.assertEquals(API_DIRECTORY_NAME, servicesMap.get(API_DIRECTORY_SERVICE_ID).get(ServicePropertyName.NAME));
 		Assert.assertEquals(API_DIRECTORY_DESCRIPTION, servicesMap.get(API_DIRECTORY_SERVICE_ID).get(ServicePropertyName.DESCRIPTION));
+		//TODO check default values of unset property
 		Assert.assertEquals(TRIPADVISOR_NAME, servicesMap.get(TRIPADVISOR_SERVICE_ID).get(ServicePropertyName.NAME));
 		Assert.assertEquals(TRIPADVISOR_DESCRIPTION, servicesMap.get(TRIPADVISOR_SERVICE_ID).get(ServicePropertyName.DESCRIPTION));
 		Assert.assertEquals(TRIPADVISOR_TAGS, servicesMap.get(TRIPADVISOR_SERVICE_ID).get(ServicePropertyName.TAGS));
@@ -49,11 +70,93 @@ public class ParserApplicationPropertiesTest {
 		Map<String, Object> service_API_DIRECTORY = new HashMap<>();
 		service_API_DIRECTORY.put("NAME", API_DIRECTORY_NAME);
 		service_API_DIRECTORY.put("DESCRIPTION", API_DIRECTORY_DESCRIPTION);
+		service_API_DIRECTORY.put("PLAN", getAPIDIRECTORYPlansProperty());
+		service_API_DIRECTORY.put("CREDENTIALS", getAPIDIRECTORYServiceCredentialsProperty());
 		services.put(API_DIRECTORY_SERVICE_ID, service_API_DIRECTORY);
 		Map<String, Object> service_TRIPADVISOR = new HashMap<>();
 		service_TRIPADVISOR.put("NAME", TRIPADVISOR_NAME);
 		service_TRIPADVISOR.put("DESCRIPTION", TRIPADVISOR_DESCRIPTION);
 		service_TRIPADVISOR.put("TAGS", TRIPADVISOR_TAGS);
+		service_TRIPADVISOR.put("METADATA", getTRIPADVISORServiceMetadataProperty());
+		service_TRIPADVISOR.put("CREDENTIALS", getTRIPADVISORServiceCredentialsProperty());
+		services.put(TRIPADVISOR_SERVICE_ID, service_TRIPADVISOR);
+		return services;
+	}
+
+	@Test
+	public void shoud_get_plans_map_with_default_info_for_the_services_have_not_set_plan_info_in_property(){
+		final PlansMap plansMap = new ParserApplicationProperties().parsePlansProperties(TRIPADVISOR_SERVICE_ID);
+		Assert.assertNotNull(plansMap);
+		Assert.assertEquals(1, plansMap.getIDs().size());
+		Assert.assertNotNull(plansMap.get(PlansMap.defaultPlanID));
+		Assert.assertEquals("default", plansMap.get(PlansMap.defaultPlanID).get(PlanPropertyName.NAME));
+		Assert.assertEquals("Default plan", plansMap.get(PlansMap.defaultPlanID).get(PlanPropertyName.DESCRIPTION));
+		Assert.assertEquals("true", plansMap.get(PlansMap.defaultPlanID).get(PlanPropertyName.FREE));
+		Assert.assertEquals("{}", plansMap.get(PlansMap.defaultPlanID).get(PlanPropertyName.METADATA));
+	}
+	
+	@Test
+	public void shoud_get_plans_map_of_specific_service_which_have_been_set_in_property(){
+		ParserApplicationProperties parserApplicationProperties = new ParserApplicationProperties();
+		parserApplicationProperties.setServices(getServicesProperty());
+		final PlansMap plansMap = parserApplicationProperties.parsePlansProperties(API_DIRECTORY_SERVICE_ID);
+		Assert.assertNotNull(plansMap);
+		Assert.assertEquals(3, plansMap.getIDs().size());
+		Assert.assertNotNull(plansMap.get(API_DIRECTORY_PLAN_PLAN1_ID));
+		Assert.assertEquals(API_DIRECTORY_PLAN_PLAN1_NAME, plansMap.get(API_DIRECTORY_PLAN_PLAN1_ID).get(PlanPropertyName.NAME));
+		Assert.assertEquals(API_DIRECTORY_PLAN_PLAN1_DESCRIPTION, plansMap.get(API_DIRECTORY_PLAN_PLAN1_ID).get(PlanPropertyName.DESCRIPTION));
+		Assert.assertEquals("true", plansMap.get(API_DIRECTORY_PLAN_PLAN1_ID).get(PlanPropertyName.FREE));
+		Assert.assertEquals("{}", plansMap.get(API_DIRECTORY_PLAN_PLAN1_ID).get(PlanPropertyName.METADATA));
+		Assert.assertNotNull(plansMap.get(API_DIRECTORY_PLAN_PLAN2_ID));
+		Assert.assertEquals(API_DIRECTORY_PLAN_PLAN2_NAME, plansMap.get(API_DIRECTORY_PLAN_PLAN2_ID).get(PlanPropertyName.NAME));
+		Assert.assertEquals("Default plan", plansMap.get(API_DIRECTORY_PLAN_PLAN2_ID).get(PlanPropertyName.DESCRIPTION));
+		Assert.assertEquals(API_DIRECTORY_PLAN_PLAN2_FREE, plansMap.get(API_DIRECTORY_PLAN_PLAN2_ID).get(PlanPropertyName.FREE));
+		Assert.assertEquals(API_DIRECTORY_PLAN_PLAN2_METADATA, plansMap.get(API_DIRECTORY_PLAN_PLAN2_ID).get(PlanPropertyName.METADATA));
+		Assert.assertNotNull(plansMap.get(API_DIRECTORY_PLAN_PLAN3_ID));
+		Assert.assertEquals(API_DIRECTORY_PLAN_PLAN3_NAME, plansMap.get(API_DIRECTORY_PLAN_PLAN3_ID).get(PlanPropertyName.NAME));
+		Assert.assertEquals("Default plan", plansMap.get(API_DIRECTORY_PLAN_PLAN3_ID).get(PlanPropertyName.DESCRIPTION));
+		Assert.assertEquals("true", plansMap.get(API_DIRECTORY_PLAN_PLAN3_ID).get(PlanPropertyName.FREE));
+		Assert.assertEquals("{}", plansMap.get(API_DIRECTORY_PLAN_PLAN3_ID).get(PlanPropertyName.METADATA));
+	}
+	
+	@Test
+	public void should_get_credentials_map_which_have_been_set_in_property(){
+		ParserApplicationProperties parserApplicationProperties = new ParserApplicationProperties();
+		parserApplicationProperties.setServices(getServicesProperty());
+		final CredentialsMap credentialsMap = parserApplicationProperties.parseCredentialsProperties();
+		Assert.assertNotNull(credentialsMap);
+		Assert.assertEquals(5, credentialsMap.getEntrySet().size());
+		Assert.assertTrue(credentialsMap.contains(TRIPADVISOR_SERVICE_ID, null, "ACCESS_KEY", TRIPADVISOR_SERVICE_CREDENTIALS_ACCESS_KEY));
+		Assert.assertTrue(credentialsMap.contains(API_DIRECTORY_SERVICE_ID, null, "HOSTNAME", API_DIRECTORY_SERVICE_CREDENTIALS_HOSTNAME));
+		Assert.assertTrue(credentialsMap.contains(API_DIRECTORY_SERVICE_ID, API_DIRECTORY_PLAN_PLAN1_ID, "ACCESS_KEY", API_DIRECTORY_PLAN_PLAN1_CREDENTIALS_ACCESS_KEY));
+		Assert.assertTrue(credentialsMap.contains(API_DIRECTORY_SERVICE_ID, API_DIRECTORY_PLAN_PLAN1_ID, "URI", API_DIRECTORY_PLAN_PLAN1_CREDENTIALS_URI));
+		Assert.assertTrue(credentialsMap.contains(API_DIRECTORY_SERVICE_ID, API_DIRECTORY_PLAN_PLAN2_ID, "ACCESS_KEY", API_DIRECTORY_PLAN_PLAN2_CREDENTIALS_ACCESS_KEY));
+		Assert.assertTrue(credentialsMap.contains(API_DIRECTORY_SERVICE_ID, API_DIRECTORY_PLAN_PLAN2_ID, "URI", API_DIRECTORY_PLAN_PLAN2_CREDENTIALS_URI));
+		Assert.assertTrue(credentialsMap.contains(API_DIRECTORY_SERVICE_ID, API_DIRECTORY_PLAN_PLAN3_ID, "ACCESS_KEY", API_DIRECTORY_PLAN_PLAN3_CREDENTIALS_ACCESS_KEY));
+		Assert.assertTrue(credentialsMap.contains(API_DIRECTORY_SERVICE_ID, API_DIRECTORY_PLAN_PLAN3_ID, "URI", API_DIRECTORY_PLAN_PLAN3_CREDENTIALS_URI));
+	}
+	
+	private Map<String, Object> getAPIDIRECTORYPlansProperty(){
+		Map<String, Object> plansProperty = new HashMap<>();
+		Map<String, Object> planOneProperty = new HashMap<>();
+		planOneProperty.put("NAME", API_DIRECTORY_PLAN_PLAN1_NAME);
+		planOneProperty.put("DESCRIPTION", API_DIRECTORY_PLAN_PLAN1_DESCRIPTION);
+		planOneProperty.put("CREDENTIALS", getAPIDIRECTORYPlanOneCredentialsProperty());
+		plansProperty.put(API_DIRECTORY_PLAN_PLAN1_ID, planOneProperty);
+		Map<String, Object> planTwoProperty = new HashMap<>();
+		planTwoProperty.put("NAME", API_DIRECTORY_PLAN_PLAN2_NAME);
+		planTwoProperty.put("FREE", API_DIRECTORY_PLAN_PLAN2_FREE);
+		planTwoProperty.put("METADATA", API_DIRECTORY_PLAN_PLAN2_METADATA);
+		planTwoProperty.put("CREDENTIALS", getAPIDIRECTORYPlanTwoCredentialsProperty());
+		plansProperty.put(API_DIRECTORY_PLAN_PLAN2_ID, planTwoProperty);
+		Map<String, Object> planThreeProperty = new HashMap<>();
+		planThreeProperty.put("NAME", API_DIRECTORY_PLAN_PLAN3_NAME);
+		planThreeProperty.put("CREDENTIALS", getAPIDIRECTORYPlanThreeCredentialsProperty());
+		plansProperty.put(API_DIRECTORY_PLAN_PLAN3_ID, planThreeProperty);
+		return plansProperty;
+	}
+	
+	private Map<String, Object> getTRIPADVISORServiceMetadataProperty(){
 		Map<String, Object> service_TRIPADVISOR_METADATA = new HashMap<>();
 		service_TRIPADVISOR_METADATA.put("DISPLAYNAME", TRIPADVISOR_METADATA_DISPLAYNAME);
 		service_TRIPADVISOR_METADATA.put("IMAGEURL", TRIPADVISOR_METADATA_IMAGEURL);
@@ -61,9 +164,40 @@ public class ParserApplicationPropertiesTest {
 		service_TRIPADVISOR_METADATA.put("DOCUMENTATIONURL", TRIPADVISOR_METADATA_DOCUMENTATIONURL);
 		service_TRIPADVISOR_METADATA.put("PROVIDERDISPLAYNAME", TRIPADVISOR_METADATA_PROVIDERDISPLAYNAME);
 		service_TRIPADVISOR_METADATA.put("LONGDESCRIPTION", TRIPADVISOR_METADATA_LONGDESCRIPTION);
-		service_TRIPADVISOR.put("METADATA", service_TRIPADVISOR_METADATA);
-		services.put(TRIPADVISOR_SERVICE_ID, service_TRIPADVISOR);
-		return services;
+		return service_TRIPADVISOR_METADATA;
 	}
-
+	
+	private Map<String, Object> getTRIPADVISORServiceCredentialsProperty(){
+		Map<String, Object> serviceCredentials = new HashMap<>();
+		serviceCredentials.put("ACCESS_KEY", TRIPADVISOR_SERVICE_CREDENTIALS_ACCESS_KEY);
+		return serviceCredentials;
+	}
+	
+	
+	private Map<String, Object> getAPIDIRECTORYServiceCredentialsProperty(){
+		Map<String, Object> serviceCredentials = new HashMap<>();
+		serviceCredentials.put("HOSTNAME", API_DIRECTORY_SERVICE_CREDENTIALS_HOSTNAME);
+		return serviceCredentials;
+	}
+	
+	private Map<String, Object> getAPIDIRECTORYPlanOneCredentialsProperty(){
+		Map<String, Object> planOneCredentials = new HashMap<>();
+		planOneCredentials.put("URI", API_DIRECTORY_PLAN_PLAN1_CREDENTIALS_URI);
+		planOneCredentials.put("ACCESS_KEY", API_DIRECTORY_PLAN_PLAN1_CREDENTIALS_ACCESS_KEY);
+		return planOneCredentials;
+	}
+	
+	private Map<String, Object> getAPIDIRECTORYPlanTwoCredentialsProperty(){
+		Map<String, Object> planTwoCredentials = new HashMap<>();
+		planTwoCredentials.put("URI", API_DIRECTORY_PLAN_PLAN2_CREDENTIALS_URI);
+		planTwoCredentials.put("ACCESS_KEY", API_DIRECTORY_PLAN_PLAN2_CREDENTIALS_ACCESS_KEY);
+		return planTwoCredentials;
+	}
+	
+	private Map<String, Object> getAPIDIRECTORYPlanThreeCredentialsProperty(){
+		Map<String, Object> planThreeCredentials = new HashMap<>();
+		planThreeCredentials.put("URI", API_DIRECTORY_PLAN_PLAN3_CREDENTIALS_URI);
+		planThreeCredentials.put("ACCESS_KEY", API_DIRECTORY_PLAN_PLAN3_CREDENTIALS_ACCESS_KEY);
+		return planThreeCredentials;
+	}
 }
