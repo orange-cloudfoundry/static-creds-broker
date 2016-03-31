25eaf2785121b430a04a02f9fae02af2fed19b81
Sebastien Bortolussi <sebastien.bortolussi@orange.com>
2016-03-31 15:49:21 +0200

    fix : set required security.user.password env var

--- src/test/java/com/orange/RemoteConfigTest.java
+++ src/test/java/com/orange/RemoteConfigTest.java
@@ -14,7 +14,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
 //"native" profile in the Config Server will not use Git, but just loads the config files from the local classpath or file system
-@WebIntegrationTest({"enable=true","spring.profiles.active=native","spring.cloud.config.server.native.searchLocations=classpath:/remote-config"})
+@WebIntegrationTest({"enable=true","security.user.password=pass","spring.profiles.active=native","spring.cloud.config.server.native.searchLocations=classpath:/remote-config"})
 public class RemoteConfigTest {
 
     @Value("${services.API_DIRECTORY.NAME}")
