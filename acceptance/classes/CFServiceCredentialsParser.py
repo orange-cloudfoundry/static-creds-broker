900c2cc214bf9c7ec32a77ab75058ef1dd59514d
tao-xinxiu <isabelle.tao@orange.com>
2016-04-11 21:48:31 +0200

    fix acceptance test of credential info when no user-provided env variable.

--- acceptance/classes/CFServiceCredentialsParser.py
+++ acceptance/classes/CFServiceCredentialsParser.py
@@ -11,7 +11,11 @@ class CFServiceCredentialsParser():
         return json.loads(cf_service_key_info)
 
     def parse_app_env_credential_info(self, cf_app_env, service_name, service_instance_name):
-        cf_app_env_sys = re.findall('System-Provided:(.*)User-Provided:', cf_app_env, re.M | re.S)[0]
+        cf_app_env_sys_list = re.findall('System-Provided:(.*)User-Provided:', cf_app_env, re.M | re.S)
+        if len(cf_app_env_sys_list) != 0:
+            cf_app_env_sys = cf_app_env_sys_list[0]
+        else:
+            cf_app_env_sys = re.findall('System-Provided:(.*)No user-defined env', cf_app_env, re.M | re.S)[0]
         cf_services_credentials_str = re.findall(r'\{\s*"VCAP_SERVICES":(.*)\}\s*\{\s*"VCAP_APPLICATION":.*\}', cf_app_env_sys, re.M | re.S)[0]
         cf_service_credentials = json.loads(cf_services_credentials_str)[service_name]
         return next(credential['credentials'] for credential in cf_service_credentials if credential['name'] == service_instance_name)
