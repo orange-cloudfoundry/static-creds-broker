190caa7f6dc2fb5485f61da2531e39e6ac2cfdb3
tao-xinxiu <isabelle.tao@orange.com>
2016-04-12 14:04:36 +0200

    create a release directory instead of making zip package, because zip command not supported in image maven

--- concourse/package.sh
+++ concourse/package.sh
@@ -4,4 +4,5 @@ set -e
 set -x 
 
 mvn package
-zip -j static-creds-broker manifest.tmpl.yml manifest.tmpl.yaml-config.yml manifest.tmpl.remote-config.yml application.tmpl.yml target/*.jar
+mkdir release/
+cp manifest.tmpl.yml manifest.tmpl.yaml-config.yml manifest.tmpl.remote-config.yml application.tmpl.yml target/*.jar release/
