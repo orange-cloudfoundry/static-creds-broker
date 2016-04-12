a918cd6da6b3368c47c82af4153fb8423dd540a1
tao-xinxiu <isabelle.tao@orange.com>
2016-04-12 14:22:37 +0200

    fix remove input path "."

--- concourse/package.sh
+++ concourse/package.sh
@@ -3,5 +3,6 @@
 set -e 
 set -x 
 
+cd static-creds-broker/
 mvn package
-cp manifest.tmpl.yml manifest.tmpl.yaml-config.yml manifest.tmpl.remote-config.yml application.tmpl.yml target/*.jar release/
+cp manifest.tmpl.yml manifest.tmpl.yaml-config.yml manifest.tmpl.remote-config.yml application.tmpl.yml target/*.jar ../release/
