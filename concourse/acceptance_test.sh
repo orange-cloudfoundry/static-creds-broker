2d1ae880652af4c4a9eaf803b92bf727cae3de34
tao-xinxiu <isabelle.tao@orange.com>
2016-04-18 13:57:03 +0200

    fix app failed to start, JAVA_OPTS -Djava.security.egd=file:///dev/urandom

--- concourse/acceptance_test.sh
+++ concourse/acceptance_test.sh
@@ -3,5 +3,6 @@
 set -e 
 set -x 
 
+export JAVA_OPTS="$JAVA_OPTS -Djava.security.egd=file:///dev/urandom"
 export binary_directory=$PWD/release/
 robot --pythonpath static-creds-broker/acceptance/classes/ --outputdir ./report/ static-creds-broker/acceptance/
