19fd123183a6950249093ec2fea6099c50f058c1
tao-xinxiu <isabelle.tao@orange.com>
2016-04-18 17:01:00 +0200

    fix JAVA_OPTS: add to manifest template

--- concourse/acceptance_test.sh
+++ concourse/acceptance_test.sh
@@ -3,6 +3,5 @@
 set -e 
 set -x 
 
-export JAVA_OPTS="-Djava.security.egd=file:///dev/urandom $JAVA_OPTS"
 export binary_directory=$PWD/release/
 robot --pythonpath static-creds-broker/acceptance/classes/ --outputdir ./report/ static-creds-broker/acceptance/
